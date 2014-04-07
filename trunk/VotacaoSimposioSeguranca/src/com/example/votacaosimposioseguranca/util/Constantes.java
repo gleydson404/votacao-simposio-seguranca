738e0c25501e585d0f2e7be2f084fd66819a9b55
gleydsonbelfort <gleydsonbelfort@cfe75d9e-a9df-5b94-991c-06be7765bb2e>
2014-04-07 23:59:22 +0000

    Inclusão dashboard e action bar

---
+++ trunk/VotacaoSimposioSeguranca/src/com/example/votacaosimposioseguranca/util/Constantes.java
@@ -0,0 +1,11 @@
+package com.example.votacaosimposioseguranca.util;
+
+public class Constantes {
+	public final static String IP_SERVER = "http://10.0.0.199";
+//	public final static String IP_SERVER = "http://10.10.1.159";
+	public final static String PORT = ":8080/";
+	public final static String CONTEXT = "VotacaoSimposioSeguranca/";
+	public final static String SERVLET = "Voto/";
+	public final static String METHOD = "InsertVoto";
+	public final static String URL = IP_SERVER + PORT + CONTEXT + SERVLET+METHOD;
+}
